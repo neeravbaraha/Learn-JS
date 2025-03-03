df2d9704bdf3fbddbe96e2db900c5158c8cc6645
neeravbaraha <neeravbaraha07@gmail.com>
2025-03-04 00:30:49 +0545

    string added

---
+++ DataType/String/string.js
@@ -0,0 +1,8 @@
+let name="NirabBaraha";
+console.log(name); // print NirabBaraha
+console.log(name.length); //print 11
+console.log(name.toUpperCase());//NIRABBARAHA
+console.log(name.toLowerCase());//nirabbaraha
+console.log(name.charAt(0));//N
+console.log(name.substring(0,5));//NIRAB
+console.log(name.includes("Baraha"));//TRUE
