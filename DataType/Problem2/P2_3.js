297ba2fb859ba9c4e1536fadcab4b718b35b7561
neeravbaraha <neeravbaraha07@gmail.com>
2025-03-04 14:22:52 +0545

    PROBLEM

---
+++ DataType/Problem2/P2_3.js
@@ -0,0 +1,12 @@
+//3.	Write a function that takes a number and returns "Even" or "Odd".
+
+function evenOrOdd(num){
+    if(num%2==0){
+        console.log("even");
+    }
+    else{
+        console.log("odd");
+    }
+
+}
+evenOrOdd(10);
